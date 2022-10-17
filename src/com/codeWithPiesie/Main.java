e889ff218efd34619693a12e7595ab9c344f38c2
piesieomane <piesieyeboah@gmail.com>
2022-10-17 10:48:32 +0000

    Change Textbox into var keyword

--- src/com/codeWithPiesie/Main.java
+++ src/com/codeWithPiesie/Main.java
@@ -3,7 +3,7 @@ package com.codeWithPiesie;
 public class Main {
     public static void main(String[] args) {
 
-        TextBox textBox1 = new TextBox();
+        var textBox1 = new TextBox();
         textBox1.setText("Hello");
         System.out.println(textBox1.text);
     }
