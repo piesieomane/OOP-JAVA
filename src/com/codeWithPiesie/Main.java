7fb08a7d4919e7056de4fad431809dc44469bfd6
piesieomane <piesieyeboah@gmail.com>
2022-10-19 10:14:40 +0000

    Remove setter for hourly and basesalary

--- src/com/codeWithPiesie/Main.java
+++ src/com/codeWithPiesie/Main.java
@@ -3,8 +3,6 @@ package com.codeWithPiesie;
 public class Main {
     public static void main(String[] args) {
         var employee = new Employee();
-        employee.setBaseSalary(50000);
-        employee.setHourlyRate(20);
         int wage = employee.calculateWage(20);
         System.out.println(wage);
     }
