6d942b781281ede5f9f3257af95643b3d61d5918
piesieomane <piesieyeboah@gmail.com>
2022-10-19 09:46:16 +0000

    create browser class

---
+++ src/com/codeWithPiesie/Browser.java
@@ -0,0 +1,17 @@
+package com.codeWithPiesie;
+
+public class Browser {
+    public void navigate (String address) {
+        String ip = findIpAddress (address);
+        String html = sendHttpRequest(ip);
+        System.out.println(html);
+    }
+
+    public String sendHttpRequest(String ip) {
+        return "<html></html>";
+    }
+
+    public String findIpAddress(String address) {
+        return "127.0.0.1";
+    }
+}
