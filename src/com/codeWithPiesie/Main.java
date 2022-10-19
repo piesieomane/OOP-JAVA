1275d79e88e287639d7845bd2272d47fbf4bcee0
piesieomane <piesieyeboah@gmail.com>
2022-10-19 09:53:59 +0000

    Temporarily remove code from main class

--- src/com/codeWithPiesie/Main.java
+++ src/com/codeWithPiesie/Main.java
@@ -2,10 +2,10 @@ package com.codeWithPiesie;
 
 public class Main {
     public static void main(String[] args) {
-        var employee = new Employee();
-        employee.setBaseSalary(50000);
-        employee.setHourlyRate(20);
-        int wage = employee.calculateWage(20);
-        System.out.println(wage);
+//        var employee = new Employee();
+//        employee.setBaseSalary(50000);
+//        employee.setHourlyRate(20);
+//        int wage = employee.calculateWage(20);
+//        System.out.println(wage);
     }
 }
